9df7482835c0000b879d81a52096e36881d014ed
brxyxnnf <brxyxnnf@gmail.com>
2019-11-27 23:56:05 -0400

    interfaz basica + funcion ordenarTexto que inierte la cadena entrante

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { summaryFileName } from '@angular/compiler/src/aot/util';
+import { SummaryResolver } from '@angular/compiler';
+import { NumericValueAccessor } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+public numeroA;
+public numeroB;
+public auxiliar_A: String="";
+public auxiliar_B: String[];
+  constructor() {
+    
+  }
 
-  constructor() {}
+  Suma(){
+
+    console.log(this.numeroA[3]);
+  }
+
+  ordenarTexto(){
+    var tamanoA:number=this.numeroA.length;    
+    while(tamanoA>=0){
+      this.auxiliar_A=this.auxiliar_A+this.numeroA[tamanoA];
+      this.auxiliar_B.push(this.numeroA[tamanoA]);
+      tamanoA--;
+    }
+    console.log(this.auxiliar_A);
+    console.log(this.auxiliar_B);
+  }
 
 }
