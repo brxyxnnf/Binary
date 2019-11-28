4436383d86ed7ad7f955230a1990e40e64dd5e00
brxyxnnf <56161860+brxyxnnf@users.noreply.github.com>
2019-11-28 09:48:29 +0100

    Suma completada

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,26 +11,81 @@ import { NumericValueAccessor } from '@ionic/angular';
 export class HomePage {
 public numeroA;
 public numeroB;
-public auxiliar_A: String="";
-public auxiliar_B: String[];
-  constructor() {
-    
-  }
+public resultado;
+public parcial;
+public acarreo = 0;
+public inversoA;
+public inversoB;
+public arriba;
+public abajo;
 
-  Suma(){
+  constructor() {}
 
-    console.log(this.numeroA[3]);
-  }
-
-  ordenarTexto(){
-    var tamanoA:number=this.numeroA.length;    
-    while(tamanoA>=0){
-      this.auxiliar_A=this.auxiliar_A+this.numeroA[tamanoA];
-      this.auxiliar_B.push(this.numeroA[tamanoA]);
-      tamanoA--;
+  Suma() {
+    this.resultado = '';
+    this.Ordenando();
+    for (let i = 0; i < this.inversoA.length; i++) {
+      if (this.acarreo === 0) {
+        this.Sumando(this.inversoA[i], this.inversoB[i]);
+      } else {
+        this.acarreo = 0;
+        this.Sumando(this.inversoA[i], this.inversoB[i]);
+        this.Sumando(this.parcial, '1');
+      }
+      this.resultado = this.resultado + this.parcial;
     }
-    console.log(this.auxiliar_A);
-    console.log(this.auxiliar_B);
+    if (this.acarreo === 1) {
+      this.resultado = this.resultado + '1';
+      this.acarreo = 0;
+    }
+    this.resultado = this.resultado.split('').reverse().join('');
+    console.log(this.resultado);
+  }
+  Sumando(num1: string, num2: string) {
+    if (num1 === '0' && num2 === '0') {
+      this.parcial = '0';
+    } else if (num1 === '1' && num2 === '1') {
+      this.parcial = '0';
+      this.acarreo = 1;
+    } else {
+      this.parcial = '1';
+    }
+  }
+  // Control del número mayor arriba
+  Resta() {}
+
+  Restando(num1: string, num2: string) {
+    if (num1 === '1' && num2 === '0') {
+      this.parcial = '1';
+    } else if (num1 === '0' && num2 === '1') {
+      this.parcial = '1';
+      this.acarreo = 1;
+    } else {
+      this.parcial = '0';
+    }
+  }
+
+  Ordenando() {
+    if (this.numeroA.length > this.numeroB.length) {
+      this.inversoA = this.numeroA.split('').reverse().join('');
+      this.inversoB = this.numeroB.split('').reverse().join('');
+    } else {
+      this.inversoA = this.numeroB.split('').reverse().join('');
+      this.inversoB = this.numeroA.split('').reverse().join('');
+    }
+    const dif = this.inversoA.length - this.inversoB.length;
+    for (let i = 0; i < dif; i++) {
+      this.inversoB = this.inversoB + '0';
+    }
+  }
+
+  Multiplicacion() {
+    this.resultado = '';
+    if (this.numeroB[0] === '1') {
+      const arriba = this.inversoA;
+      const abajo = '0';
+      this.Suma();
+    }
   }
 
 }
